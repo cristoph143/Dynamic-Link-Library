72fa8737724357070c53913edb6e86fee28e1127
Cristopher Bohol <cris.bacus6@gmail.com>
2024-09-04 22:06:05 +0800

    Module 2: Working with DLLs

---
+++ MathClient/ToolCommon.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <map>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+#define PRINT_MESSAGE(format, ...) printf(format "\n", ##__VA_ARGS__)
+#define PRINT_ERROR_MESSAGE(message) std::cerr << "Error: " << message << std::endl
+
+// Function pointer typedefs for dynamic functions
+typedef void (*MenuAction)();
+
+// Struct to hold menu option description and associated action
+struct MenuOption {
+    string description;
+    MenuAction action;
+};
+
+// Typedefs for function pointers
+typedef void (*FnReverseString)(char*);
+typedef void (*FnToUpperCase)(char*);
+typedef void (*FnReadFromFile)(const char*);
+typedef void (*FnWriteToFile)(const char*, const char*);
+
+template<typename T>
+T loadFunction(HMODULE hLib, const char* functionName) {
+    // Get the address of the function
+    auto function = reinterpret_cast<T>(GetProcAddress(hLib, functionName));
+    if (!function) {
+        cerr << "Failed to load function: " << functionName << endl;
+    }
+    return function;
+}
+
+//void runMenu(const map<int, MenuOption>& menuOptions);
+void runMenu(const map<int, MenuOption>& menuOptions, int exitOption);
+
+void exitApp();
+
+string getStringInput(const std::string& prompt);
